4b8782139b6fd3c5bf00a24a5eddea1fe0256124
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-06-14 11:03:40 -0300

    Added favicons (bash logo)

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,6 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8">
         <title>Excuser 99</title>
+        <link rel="icon" type="image/png" sizes="16x16" href='{{ asset("favicon-16x16.png") }}'>
+        <link rel="icon" type="image/png" sizes="32x32" href='{{ asset("favicon-32x32.png") }}'>
+        <link rel="apple-touch-icon" sizes="180x180" href='{{ asset("apple-touch-icon.png") }}'>
         @stack('head')
     </head>
     <body class='font-mono text-green-500 bg-zinc-900'>
