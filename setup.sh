c3116fcafcf3398aae1f82bbff9dbb7260f8e04b
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-08-13 20:52:57 -0300

    Added URL, PORT and HOST as flags for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 # Safety guard to quit execution at first command error
-set -e
+set -euo pipefail
+
+# Sets URL and PORT
+URL="$1"
+PORT="${2:-8000}"
+HOST="${3:-0.0.0.0}"
+
+if [ -z "$URL" ]; then
+    echo "Error: Missing required URL parameter."
+    echo "Usage: $0 <URL> [PORT] [HOST]"
+    exit 1
+fi
 
 # Dependencies
 apt update && \
@@ -9,7 +20,7 @@ COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader && \
 npm ci && npm run build && \
 
 # .env File
-([ ! -f .env ] &&  cp .env.example .env && php artisan key:generate && sed -i "/s/APP_URL/APP_URL=http://0.0.0.0:8000/" .env || true) && \
+([ ! -f .env ] &&  cp .env.example .env && php artisan key:generate && sed -i "|s|APP_URL=.*|APP_URL=$URL|g" .env || true) && \
 
 # Database
 mkdir -p database && \
@@ -24,4 +35,4 @@ php artisan route:cache && \
 php artisan view:cache && \
 
 # Serving web page
-php -d variables_order=EGPCS artisan serve --host=0.0.0.0 --port=8000 2>&1
+php -d variables_order=EGPCS artisan serve --host="$HOST" --port="$PORT" 2>&1
